ea223a2c14279bb6c1a8d002229b5e484db488aa
Jasper Bailey <jasperbailey98@gmail.com>
2024-01-09 16:17:26 +0000

    definitely fixed CORS

--- scheduler/app.py
+++ scheduler/app.py
@@ -36,6 +36,11 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+        },
         "body": json.dumps(
             {"message": "Schedule calculated successfully.", "schedule": schedule}
         ),
